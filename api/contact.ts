da90f01a0e2652cac5d2a2b6218dc45caa7f88e1
Mulatu Mekonnin <Mulatu.Mekonnin@nibbanksc.com>
2026-03-10 13:20:40 +0300

    update project configuration

--- api/contact.ts
+++ api/contact.ts
@@ -2,6 +2,8 @@ import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
+export const config = { runtime: "nodejs18.x" };
+
 export default async function handler(req: any, res: any) {
   if (req.method === 'POST') {
     const { name, email, message, subject } = req.body
